7691035b6e0a8499a2a71a87bbb9a849636cc8e0
thigh burger <lewis177777@gmail.com>
2024-09-09 16:37:55 +0900

    Variable and Data Type Exercises

---
+++ practice/practice/week2/char_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1="GOOD";
+    string s2="Morning";
+    string s3=s1+" "+s2+ "!";
+    cout<<s3<<endl;
+    
+}
---
+++ practice/practice/week2/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s1="GOOD";
+    string s2="Bad";
+    bool b = (s1==s2);
+    cout<<b<<endl;
+    s2="GOOD";
+    b=(s1==s2);
+    cout <<b<<endl;
+}
